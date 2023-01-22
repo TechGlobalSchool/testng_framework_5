5e148b625abf8b39bfa09d0c5fd27118e7d29cdb
TechGlobalSchool <tgschool01@gmail.com>
2023-01-22 13:01:11 -0600

    added Profiles

---
+++ src/test/java/utilities/DropdownHandler.java
@@ -0,0 +1,19 @@
+package utilities;
+
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class DropdownHandler {
+
+    //Create a method that takes a dropdown, dropdown options and optionText that you want to click
+    public static void clickOnDropdownOption(WebElement dropdown, List<WebElement> dropdownOptions, String optionText){
+        dropdown.click();
+        for (WebElement dropdownOption : dropdownOptions) {
+            if(dropdownOption.getText().equals(optionText)){
+                dropdownOption.click();
+                break;
+            }
+        }
+    }
+}
