4ce6a8b6952fc6fa2411d65f905aab0336d51a86
TechGlobalSchool <tgschool01@gmail.com>
2023-01-29 11:57:25 -0600

    dynamic table

--- src/test/java/scripts/TechGlobalDynamicTablesTest.java
+++ src/test/java/scripts/TechGlobalDynamicTablesTest.java
@@ -6,7 +6,10 @@ import org.testng.annotations.Test;
 import pages.TechGlobalAlertsPage;
 import pages.TechGlobalDynamicTablesPage;
 import pages.TechGlobalFrontendTestingHomePage;
+import utilities.TableData;
 import utilities.Waiter;
+
+import java.util.stream.IntStream;
 
 public class TechGlobalDynamicTablesTest extends TechGlobalBase{
 
@@ -32,5 +35,46 @@ public class TechGlobalDynamicTablesTest extends TechGlobalBase{
         techGlobalDynamicTablesPage.addProductButton.click();
         Assert.assertEquals(techGlobalDynamicTablesPage.modalCardTitle.getText(), "Add New Product");
         Waiter.pause(3);
+    }
+
+    /**
+     * Go to https://techglobal-training.netlify.app/
+     * Click on “Practices” dropdown in the header
+     * Select the “Frontend Testing” option
+     * Click on the “Dynamic Tables” card
+     * Click the “Add Product” button
+     * Enter “2” in the Quantity field
+     * Enter “Apple Watch” in the Product field
+     * Enter “500” in the Price $ field
+     * Click the “Submit” button
+     * Verify that a new row has been added to the table with the values “2”, “Apple Watch, “500” and the calculated Total $ value is “1,000"
+     * Verify that the Total $ value at the bottom of the table has been updated to reflect the total cost of all the products in the table, including the newly added one.
+     */
+
+    @Test(priority = 2, description = "Validate dynamic table")
+    public void validateDynamicTable(){
+        techGlobalDynamicTablesPage.addProductButton.click();
+
+        int initialTotal = Integer.parseInt(techGlobalDynamicTablesPage.totalAmount.getText().replaceAll("[^0-9]", ""));
+
+        Assert.assertEquals(techGlobalDynamicTablesPage.modalCardTitle.getText(), "Add New Product");
+
+        String[] products = {"2", "Apple Watch", "500"};
+
+        int tableRowSize = techGlobalDynamicTablesPage.tableRow.size();
+
+        Assert.assertEquals(techGlobalDynamicTablesPage.tableRow.size(), 3);
+
+        int myProductTotal = Integer.parseInt(products[0]) * Integer.parseInt(products[2]);
+
+        IntStream.range(0, products.length).forEach(i -> techGlobalDynamicTablesPage.productDetails.get(i).sendKeys(products[i]));
+
+        techGlobalDynamicTablesPage.submitButton.click();
+
+        Assert.assertEquals(techGlobalDynamicTablesPage.tableRow.size(), tableRowSize + 1);
+
+        int productTotal = Integer.parseInt(TableData.getTableRow(driver, 4).get(3).getText().replaceAll("[^0-9]", ""));
+
+
     }
 }
