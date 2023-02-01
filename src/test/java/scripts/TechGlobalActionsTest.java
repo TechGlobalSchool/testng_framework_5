4db43119dfe7606fb1b45892825904a024fb791f
TechGlobalSchool <tgschool01@gmail.com>
2023-01-31 19:53:52 -0600

    drag and drop actions

--- src/test/java/scripts/TechGlobalActionsTest.java
+++ src/test/java/scripts/TechGlobalActionsTest.java
@@ -1,10 +1,12 @@
 package scripts;
 
 import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.TechGlobalActionsPage;
 import pages.TechGlobalFrontendTestingHomePage;
+import utilities.Waiter;
 
 public class TechGlobalActionsTest extends TechGlobalBase{
 
@@ -36,6 +38,40 @@ public class TechGlobalActionsTest extends TechGlobalBase{
     @Test(priority = 1, description = "Click first 3 boxes and validate the results")
     public void clickActions(){
         actions.moveToElement(techGlobalActionsPage.clickBox).click().perform();
+        Waiter.pause(2);
+        Assert.assertEquals(techGlobalActionsPage.clickResult.getText(), "You clicked on a button!");
 
+        actions.moveToElement(techGlobalActionsPage.rightClickBox).contextClick().perform();
+        Waiter.pause(2);
+
+        Assert.assertEquals(techGlobalActionsPage.rightClickResult.getText(), "You right-clicked on a button!");
+
+        actions.moveToElement(techGlobalActionsPage.doubleClickBox).doubleClick().perform();
+        Waiter.pause(2);
+
+        Assert.assertEquals(techGlobalActionsPage.doubleClickResult.getText(), "You double-clicked on a button!");
+    }
+
+    /**
+     * TEST2
+     * Go to https://techglobal-training.netlify.app/
+     * Click on the "Practices" dropdown in the header
+     * Select the "Frontend Testing" option from the dropdown menu
+     * Click on the "Actions" card
+     * Verify that the last two web elements are present and labeled as "Drag Me", and "Drop Here",
+     * Perform a Drag and Drop action on the "Drag Me" web element, and drop it to "Drop Here"
+     * Verify that the first web element "Drag me" is successfully dropped into the second web element "Drop Here"
+     * Verify that a message appears next to the  web element stating "An element dropped here!"
+     */
+
+    //Move to an element, click and hold the element, and move it to Drop Here, and release it there
+
+    @Test(priority = 2, description = "Drag and Drop action")
+    public void dragAndDropAction(){
+        actions.moveToElement(techGlobalActionsPage.dragMeBox).clickAndHold()
+                .moveToElement(techGlobalActionsPage.dropHereBox).release().perform();
+
+        Waiter.waitForVisibilityOfElement(techGlobalActionsPage.dragAndDropResult, 10);
+        Assert.assertEquals(techGlobalActionsPage.dragAndDropResult.getText(), "An element dropped here!");
     }
 }
